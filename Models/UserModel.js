91e981955c5d9ba1caaa941e7a6c1115d3f104b7
Ansul <ansulgurawalia@gmail.com>
2024-04-08 10:50:32 +0530

    Added login Controller

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs"; 
+import JWT from "jsonwebtoken";
 
 //schema
 const userSchema=new mongoose.Schema({ 
@@ -32,5 +33,15 @@ userSchema.pre('save', async function() {
   const salt= await bcrypt.genSalt(10); 
   this.password = await bcrypt.hash(this.password,salt); 
 });
+
+//json web token
+userSchema.methods.createJWT=function(){ 
+  return JWT.sign({userId:this._id},process.env.JWT_SECRET,{expiresIn:'1d'})
+}
+
+userSchema.methods.comparePassword=async function (password){ 
+  const isMatch=bcrypt.compare(password,this.password); 
+  return isMatch;
+}
 
 export default mongoose.model("User", userSchema);
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import userModel from "../models/userModel.js";
 
-const authController=async(req,res)=>{ 
+export const authController=async(req,res)=>{ 
     try{ 
     const {name,email,password}= req.body;  
     if(!name){ 
@@ -35,10 +35,18 @@ const authController=async(req,res)=>{
     }
 
     const user= await userModel.create({name,email,password});
+
+    //token 
+    const token=user.createJWT();
     return res.status(201).send({ 
         message:"User created succesfully ", 
         success:true,
-        user
+        user:{ 
+            name:user.name, 
+            email:user.email, 
+            location: user.location
+        },
+        token
     })
 
    }
@@ -52,4 +60,58 @@ const authController=async(req,res)=>{
    }
 } 
 
-export default authController;
+export const loginController = async (req,res)=>{ 
+   try {
+    const {email,password} = req.body; 
+    if(!email){ 
+        return res.status(200).send({ 
+            message:"Email is required for login..", 
+            success:"false"
+        })
+    }
+    if(!password){ 
+        return res.status(200).send({ 
+            message:"Password is required for login..", 
+            success:"false"
+        })
+    }
+
+    const user= await userModel.findOne({email}); 
+
+    if(!user){ 
+        res.status(200).send({ 
+            message:"Invalid credentials please login in again ", 
+            success:"false"
+        })
+    }
+
+    const isMatch = user.comparePassword(password); 
+
+    if(!isMatch){ 
+        res.status(200).send({ 
+            message:"Invalid credentials please login in again ", 
+            success:"false"
+        })
+    }
+
+    const token=user.createJWT(); 
+
+    res.status(200).json({ 
+        success:"true", 
+        message:"Login Successfully", 
+        user, 
+        token,
+    })
+    
+
+   } catch (error) {
+     res.status(400).send({ 
+        message:"errow while login ....", 
+        success:"false", 
+        error
+     })
+   }
+ 
+
+}; 
+
